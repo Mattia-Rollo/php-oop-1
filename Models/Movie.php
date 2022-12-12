08f2b0da1805d75620ddbb3a20f83493048db309
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2022-12-12 14:48:12 +0100

    add method getMovieInfo

--- Models/Movie.php
+++ Models/Movie.php
@@ -2,44 +2,51 @@
 
 class Movie
 {
-    private $title;
+    public $title;
     // public $cast;
-    public $genre;
+    private $genres;
 
 
-    public $durata;
+    private $durata;
+    private $strDurata;
 
-    public $colors;
+    private $colors;
 
     public $muted;
     public $dateRelase;
     public $budget;
 
 
-    function __construct(string $_title, string $_genre, int $_durata, string $_budget, bool $_muted)
+    function __construct(string $_title, array $_genres, int $_durata, string $_budget, bool $_muted)
     {
 
         $this->title = $_title;
         // $this->cast = $_cast;
-        $this->genre = $_genre;
+        $this->genres = $_genres;
         $this->durata = $_durata;
-
         $this->budget = $_budget;
         $this->muted = $_muted;
+        $this->durataString();
 
     }
+
+
+    // funzione per determinare se è un lungo metraggio uno medio o corto
     public function durataString()
     {
         if ($this->durata > 130) {
-            echo "è un lungometraggio";
+            $this->strDurata = "è un lungometraggio";
         } else {
-            echo "è un cortometraggio";
+            $this->strDurata = "è un cortometraggio";
         }
     }
-// funzione che capisce se è un lungo metraggio uno medio o corto
+    public function getMovieInfo()
+    {
+
+        echo " titolo: " . $this->title . " genere: " . implode(",", $this->genres) . " durata: " . $this->strDurata . "<br>";
 
 
-//
+    }
 }
 
 ?>
--- index.php
+++ index.php
@@ -2,17 +2,27 @@
 
 include __DIR__ . './Models/Movie.php';
 
-$Avatar = new Movie("Avatar", "Fantascienza", 190, "1000000", false);
+$Avatar = new Movie("Avatar", ["Fantascienza"], 190, "1000000", false);
 
 // $Avatar->title = "BelloCiccio";
 
-$Avenger = new Movie("Avenger", "Fantascienza", 130, "500000", "false");
+$Avenger = new Movie("Avenger", ["Fantascienza", "azione"], 130, "500000", "false");
 
-$BlackAdam = new Movie("BlackAdam", "Fantascienza", 100, "23456", false);
+$BlackAdam = new Movie("BlackAdam", ["Fantascienza", "Azione", ""], 100, "23456", false);
 
-var_dump($Avatar);
-$Avatar->durataString();
-var_dump($Avenger);
-$Avenger->durataString();
+// var_dump($Avatar);
+// $Avatar->durataString();
+// var_dump($Avenger);
+// $Avenger->durataString();
 var_dump($BlackAdam);
-$BlackAdam->durataString();
+// $BlackAdam->durataString();
+$Avatar->getMovieInfo();
+$Avenger->getMovieInfo();
+$BlackAdam->getMovieInfo();
+
+// $listMovie = [$Avatar, $Avenger, $BlackAdam];
+// var_dump($listMovie);
+
+// foreach ($listMovie as $movie) {
+//     echo "il film: $movie->durataString() <br>";
+// }
