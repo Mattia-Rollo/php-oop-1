e836f1f045c24e5020ad8d4e3f2e0758cc0c81af
Mattia-Rollo-Portatile <ste.tia@hotmail.it>
2022-12-12 13:06:32 +0100

    add some properties

--- Models/Movie.php
+++ Models/Movie.php
@@ -6,17 +6,26 @@ class Movie
     // public $cast;
     public $genre;
 
+
     public $durata;
 
     public $colors;
 
-    function __construct(string $_title, string $_genre, string $_durata)
+    public $muted;
+    public $dateRelase;
+    public $budget;
+
+
+    function __construct(string $_title, string $_genre, string $_durata, string $_budget, bool $_muted)
     {
 
         $this->title = $_title;
         // $this->cast = $_cast;
         $this->genre = $_genre;
         $this->durata = $_durata;
+
+        $this->budget = $_budget;
+        $this->muted = $_muted;
 
     }
 
--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 
 include __DIR__ . './Models/Movie.php';
 
-$Avatar = new Movie("Avatar", "Fantascienza", "190 minuti");
+$Avatar = new Movie("Avatar", "Fantascienza", "190 minuti", "1000000", false);
 
 // $Avatar->title = "BelloCiccio";
 
-$Avenger = new Movie("Avenger", "Fantascienza", "130 minuti");
-$BlackAdam = new Movie("BlackAdam", "Fantascienza", "100 minuti");
+$Avenger = new Movie("Avenger", "Fantascienza", "130 minuti", "500000", "false");
+
+$BlackAdam = new Movie("BlackAdam", "Fantascienza", "100 minuti", "23456", false);
 
 var_dump($Avatar);
 var_dump($Avenger);
